e39d23837447a147e5e8b81760a3e783f175e913
MCangelov <milen.chernev.angelov@gmail.com>
2023-10-12 15:42:40 +0300

    Added some exceptions

--- beach_scrapper.py
+++ beach_scrapper.py
@@ -2,86 +2,143 @@ import re
 import time
 
 import pandas as pd
+from typing import List
+
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
+
+def navigate_to_url(driver: WebDriver, url: str, wait_time: int = 10) -> None:
+    """
+    Navigates the webdriver to a specified URL and waits until the page is loaded.
+
+    Parameters:
+    driver (webdriver): The webdriver instance to use.
+    url (str): The URL to navigate to.
+    wait_time (int, optional): The maximum time to wait for the page to load. Defaults to 10.
+
+    """
+    try:
+        driver.get(url)
+        WebDriverWait(driver, wait_time).until(
+            EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    except TimeoutException:
+        raise RuntimeError(
+            f"Timeout error while navigating to {url}. Page did not load successfully.")
+    except Exception as e:
+        raise RuntimeError(
+            f"An error occurred while navigating to {url}: {str(e)}")
+
+
+def scroll_to_bottom(driver: WebDriver) -> None:
+    """
+    Scrolls the webdriver to the bottom of the page.
+    Will keep scrolling down the page until it cannot scroll any further.
+
+    Parameters:
+    driver (webdriver): The webdriver instance to use.
+
+    """
+    initial_page_source = driver.page_source
+    can_scroll = True
+    while can_scroll:
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        page_source_after_scroll = driver.page_source
+        if initial_page_source == page_source_after_scroll:
+            can_scroll = False
+        else:
+            initial_page_source = page_source_after_scroll
+
+
+def remove_elements(driver: WebDriver, css_selectors: List[str]) -> None:
+    """
+    Removes elements from the webpage that match any of the provided CSS selectors.
+
+    Parameters:
+    driver (webdriver): The webdriver instance to use.
+    css_selectors (list[str]): A list of CSS selectors. Each element that matches any of these selectors will be removed.
+
+    """
+    for css_selector in css_selectors:
+        try:
+            element = driver.find_element(By.CSS_SELECTOR, css_selector)
+            driver.execute_script("arguments[0].remove();", element)
+        except (NoSuchElementException, TimeoutException):
+            pass
+
 
 ROOT_URL = 'https://pochivka.bg/plazhove-bulgaria-f120'
+CSS_ELEMENTS = ["div.fixed-box.quiz", 'div.backdrop[style*="display: block;"]']
+PATTERN = r'span class="map">\s*<img alt="([^"]*)"'
+REMOVE_STRING = ' (плаж)'
 
+# Options are set for driver to run in the background
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(options=chrome_options)
 
-driver = webdriver.Chrome(options = chrome_options) 
-def anti_ad(url):    
-    driver = webdriver.Chrome(options = chrome_options) 
-    driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-    
-    scroll_count = 0
-    while scroll_count < 6:   
-        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
-        time.sleep(1)   
-        scroll_count += 1
+navigate_to_url(driver, ROOT_URL)
+scroll_to_bottom(driver)
+remove_elements(driver, CSS_ELEMENTS)
 
-    POPUP_ELEMENT = driver.find_element(By.CSS_SELECTOR, "div.fixed-box.quiz") 
-    driver.execute_script("arguments[0].remove();", POPUP_ELEMENT)
-
-    
-    BACKDROP_ELEMENT = driver.find_element(By.CSS_SELECTOR, 'div.backdrop[style*="display: block;"]')
-    driver.execute_script("arguments[0].remove();", BACKDROP_ELEMENT)  
-    return driver
-
-
-ROOT_DRIVER = anti_ad(ROOT_URL)
-driver.quit()
-
-ROOT_PAGE_HTML = ROOT_DRIVER.page_source
-soup = BeautifulSoup(ROOT_PAGE_HTML, "lxml")
-
-PATTERN = r'span class="map">\s*<img alt="([^"]*)"'
+soup = BeautifulSoup(driver.page_source, "lxml")
 beach_list = re.findall(PATTERN, str(soup))
-
-REMOVE_STRING = ' (плаж)'
 beach_list = [item.replace(REMOVE_STRING, '') for item in beach_list]
 beach_dict = {'beach_name': beach_list}
-TITLE_DIVS = ROOT_DRIVER.find_elements(By.CSS_SELECTOR, "div.title")
+# Needed to locate the beach URLs in the HTML
+title_divs = driver.find_elements(By.CSS_SELECTOR, "div.title")
 
-url_bank = []
-for i, title_div in enumerate(TITLE_DIVS):
+# Gathering the URLs of every beach
+utl_storage = []
+for i, title_div in enumerate(title_divs):
     try:
         inner_anchor = title_div.find_element(By.CSS_SELECTOR, "a")
         href_value = inner_anchor.get_attribute("href")
-        url_bank.append(href_value)
-        
-    except:
+        utl_storage.append(href_value)
+
+    except NoSuchElementException:
         break
 
-beach_dict['urls'] = url_bank
+beach_dict['urls'] = utl_storage
 
 latitude_container = []
 longitude_container = []
 
-for name_link in beach_dict['urls']:
-    try:
-        beach_driver = anti_ad(name_link)
-        latitude_element = beach_driver.find_element(By.CSS_SELECTOR, 'meta[property="place:location:latitude"]')
-        longitude_element = beach_driver.find_element(By.CSS_SELECTOR, 'meta[property="place:location:longitude"]')
+# Extracting the latitude and longitude of every beach
+try:
+    for name_link in beach_dict['urls']:
+        try:
+            navigate_to_url(driver, name_link)
+            scroll_to_bottom(driver)
+            remove_elements(driver, CSS_ELEMENTS)
 
-        latitude_container.append(latitude_element.get_attribute("content"))
-        longitude_container.append(longitude_element.get_attribute("content"))
-        driver.quit()
-    except TimeoutException:
-        driver.quit()
-        raise RuntimeError(f"{name_link} is broken or the required elements were not found")
+            latitude_element = driver.find_element(
+                By.CSS_SELECTOR, 'meta[property="place:location:latitude"]')
+            longitude_element = driver.find_element(
+                By.CSS_SELECTOR, 'meta[property="place:location:longitude"]')
 
-beach_dict.update({'latitude': latitude_container, 'longitude': longitude_container})
+            latitude_container.append(
+                latitude_element.get_attribute("content"))
+            longitude_container.append(
+                longitude_element.get_attribute("content"))
+        except TimeoutException:
+            raise RuntimeError(
+                f"Timeout error while processing {name_link}. The required elements were not found.")
+finally:
+    driver.quit()
+
+beach_dict.update({'latitude': latitude_container,
+                  'longitude': longitude_container})
 
 beach_info = pd.DataFrame(beach_dict)
-beach_info.to_csv('beach_info.csv', index = True)
+beach_info.to_csv('beach_info.csv', index=True)
